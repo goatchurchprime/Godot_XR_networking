3359cbc1ff12ad0beffaecd6868fa166f815e623
Julian Todd <julian@goatchurch.org.uk>
2022-03-05 19:44:32 +0000

    add the key_m for making the NetworkGateway dialog to show up

--- Main.gd
+++ Main.gd
@@ -1,6 +1,6 @@
 extends Spatial
 
-onready var NetworkGateway = $Viewport2Din3D/Viewport/NetworkGateway
+onready var NetworkGateway = $ViewportNetworkGateway/Viewport/NetworkGateway
 
 func _ready():
 	#var brokeraddress = "ws://broker.mqttdashboard.com:8000"
@@ -17,3 +17,21 @@ func _ready():
 		OS.vsync_enabled = false
 		Engine.iterations_per_second= 90
 		set_process(false)
+
+	$ARVROrigin/ARVRController_Right.connect("button_pressed", self, "vr_right_button_pressed")
+
+const VR_BUTTON_BY = 1
+func vr_right_button_pressed(button: int):
+	print("vr right button pressed ", button)
+	if button == VR_BUTTON_BY:
+		if $ViewportNetworkGateway.visible:
+			$ViewportNetworkGateway.visible = false
+		else:
+			var headtrans = $ARVROrigin/ARVRCamera.global_transform
+			$ViewportNetworkGateway.transform = Transform(headtrans.basis, headtrans.origin + headtrans.basis.z*-3)
+			$ViewportNetworkGateway.visible = true
+			
+		
+func _input(event):
+	if event is InputEventKey and event.scancode == KEY_M and event.pressed and not event.echo:
+		vr_right_button_pressed(VR_BUTTON_BY)
