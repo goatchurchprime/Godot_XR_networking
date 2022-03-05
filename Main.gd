2c0936f6aa886e15ce942e0e05138ab8d5524429
Julian Todd <julian@goatchurch.org.uk>
2022-03-05 18:44:11 +0000

    remote keyboard and prepare for network gateway to be an on off control panel

--- Main.gd
+++ Main.gd
@@ -1,6 +1,14 @@
 extends Spatial
 
+onready var NetworkGateway = $Viewport2Din3D/Viewport/NetworkGateway
+
 func _ready():
+	#var brokeraddress = "ws://broker.mqttdashboard.com:8000"
+	#var brokeraddress = "broker.mqttdashboard.com"
+	var roomname = "cucumber"
+	var brokeraddress = "mqtt.dynamicdevices.co.uk"
+	NetworkGateway.initialstatemqttwebrtc(NetworkGateway.NETWORK_OPTIONS_MQTT_WEBRTC.AS_NECESSARY, roomname, brokeraddress)
+	
 	print("  Available Interfaces are %s: " % str(ARVRServer.get_interfaces()));
 	var interface = ARVRServer.find_interface("OVRMobile")
 	if interface and interface.initialize():
@@ -8,3 +16,4 @@ func _ready():
 		get_viewport().arvr = true
 		OS.vsync_enabled = false
 		Engine.iterations_per_second= 90
+		set_process(false)
