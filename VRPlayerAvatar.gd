9efbb68451990e6bbe9744e37296fce88d5525f9
Julian Todd <julian@goatchurch.org.uk>
2022-07-12 19:39:03 +0100

    controllers are now rendered as an option and can switch from hands

--- VRPlayerAvatar.gd
+++ VRPlayerAvatar.gd
@@ -9,8 +9,8 @@ onready var LeftHandController = arvrorigin.get_node("LeftHandController")
 onready var RightHandController = arvrorigin.get_node("RightHandController")
 onready var Left_hand = arvrorigin.get_node_or_null("Left_hand")
 onready var Right_hand = arvrorigin.get_node_or_null("Right_hand")
-
-
+onready var Configuration = arvrorigin.get_node_or_null("Configuration")
+onready var XRPose = arvrorigin.get_node_or_null("XRPose")
 	
 
 var ovrbonemapping = { 2:"Wrist/ThumbMetacarpal", 3:"Wrist/ThumbMetacarpal/ThumbProximal", 4:"Wrist/ThumbMetacarpal/ThumbProximal/ThumbDistal", 
@@ -43,8 +43,17 @@ func processtoovrhand(xrhand, ovrskel, refl):
 		var qbonepose = bonerest.basis.get_rotation_quat().inverse()*qxrbonetransR
 		ovrskel.set_bone_pose(boneid, Transform(qbonepose))
 
-var leftthumbindextouched = false
-var rightthumbindextouched = false
+#var leftthumbindextouched = false
+		#var grip = controller.get_joystick_axis(JOY_VR_ANALOG_GRIP)
+		#var trigger = controller.get_joystick_axis(JOY_VR_ANALOG_TRIGGER)
+		#var thumbtippos = Left_hand.get_node("Wrist/ThumbMetacarpal/ThumbProximal/ThumbDistal/ThumbTip").global_transform.origin
+		#var indextippos = Left_hand.get_node("Wrist/IndexMetacarpal/IndexProximal/IndexIntermediate/IndexDistal/IndexTip").global_transform.origin
+		#if not leftthumbindextouched:
+		#	leftthumbindextouched = (thumbtippos.distance_to(indextippos) < 0.01)
+		#	leftthumbindexjusttouched = leftthumbindextouched
+		#else:
+		#	leftthumbindextouched = (thumbtippos.distance_to(indextippos) < 0.02)
+
 func processlocalavatarposition(delta):
 	transform = arvrorigin.transform
 	$HeadCam.transform = arvrorigin.get_node("ARVRCamera").transform
@@ -53,69 +62,52 @@ func processlocalavatarposition(delta):
 	var rightthumbindexjusttouched = false
 	
 	if LeftHandController.get_is_active():
-		$HandLeft.transform = LeftHandController.transform
-		$HandLeft/LeftHand/AnimationTree.set("parameters/Trigger/blend_amount", arvrorigin.get_node("LeftHandController/LeftHand/AnimationTree").get("parameters/Trigger/blend_amount"))
-		$HandLeft/LeftHand/AnimationTree.set("parameters/Grip/blend_amount", arvrorigin.get_node("LeftHandController/LeftHand/AnimationTree").get("parameters/Grip/blend_amount"))
+		$HandLeftOVR.visible = false
+		$ControllerLeft.transform = LeftHandController.transform
+		$ControllerLeft.visible = true
+		print(Configuration.get_tracking_confidence(1), Configuration.get_tracking_confidence(2), XRPose.get_tracking_confidence())
 	elif is_instance_valid(Left_hand) and handtrackingavailable and Left_hand.is_active():
-		$HandLeft.transform = Transform(Left_hand.transform.basis.rotated(Left_hand.transform.basis.z, deg2rad(-90)), Left_hand.transform.origin)
-		var thumbtippos = Left_hand.get_node("Wrist/ThumbMetacarpal/ThumbProximal/ThumbDistal/ThumbTip").global_transform.origin
-		var indextippos = Left_hand.get_node("Wrist/IndexMetacarpal/IndexProximal/IndexIntermediate/IndexDistal/IndexTip").global_transform.origin
-		var middletippos = Left_hand.get_node("Wrist/MiddleMetacarpal/MiddleProximal/MiddleIntermediate/MiddleDistal/MiddleTip").global_transform.origin
-		$HandLeft/LeftHand/AnimationTree.set("parameters/Trigger/blend_amount", max(0.0, 1.0 - thumbtippos.distance_to(indextippos)/0.04))
-		$HandLeft/LeftHand/AnimationTree.set("parameters/Grip/blend_amount", max(0.0, 1.0 - thumbtippos.distance_to(middletippos)/0.04))
-
-		$HandLeftOVR.transform = Left_hand.transform  # $HandRight.transform
+		$ControllerLeft.visible = false
+		$HandLeftOVR.transform = Left_hand.transform
 		processtoovrhand(Left_hand, $HandLeftOVR/ovr_left_hand_model/ArmatureLeft/Skeleton, true)
-
-		if not leftthumbindextouched:
-			leftthumbindextouched = (thumbtippos.distance_to(indextippos) < 0.01)
-			leftthumbindexjusttouched = leftthumbindextouched
-		else:
-			leftthumbindextouched = (thumbtippos.distance_to(indextippos) < 0.02)
-
-		
+		$HandLeftOVR.visible = true
+		print(Configuration.get_tracking_confidence(1), Configuration.get_tracking_confidence(2), XRPose.get_tracking_confidence())
+	else:
+		$HandLeftOVR.visible = false
+		$ControllerLeft.visible = false
+			
 	if RightHandController.get_is_active():
-		$HandRight.transform = RightHandController.transform
-		$HandRight/RightHand/AnimationTree.set("parameters/Trigger/blend_amount", arvrorigin.get_node("RightHandController/RightHand/AnimationTree").get("parameters/Trigger/blend_amount"))
-		$HandRight/RightHand/AnimationTree.set("parameters/Grip/blend_amount", arvrorigin.get_node("RightHandController/RightHand/AnimationTree").get("parameters/Grip/blend_amount"))
+		$HandRightOVR.visible = false
+		$ControllerRight.transform = RightHandController.transform
+		$ControllerRight.visible = true
 	elif is_instance_valid(Right_hand) and handtrackingavailable and Right_hand.is_active():
-		$HandRight.transform = Transform(Right_hand.transform.basis.rotated(Right_hand.transform.basis.z, deg2rad(90)), Right_hand.transform.origin)
-		var thumbtippos = Right_hand.get_node("Wrist/ThumbMetacarpal/ThumbProximal/ThumbDistal/ThumbTip").global_transform.origin
-		var indextippos = Right_hand.get_node("Wrist/IndexMetacarpal/IndexProximal/IndexIntermediate/IndexDistal/IndexTip").global_transform.origin
-		var middletippos = Right_hand.get_node("Wrist/MiddleMetacarpal/MiddleProximal/MiddleIntermediate/MiddleDistal/MiddleTip").global_transform.origin
-		$HandRight/RightHand/AnimationTree.set("parameters/Trigger/blend_amount", max(0.0, 1.0 - thumbtippos.distance_to(indextippos)/0.04))
-		$HandRight/RightHand/AnimationTree.set("parameters/Grip/blend_amount", max(0.0, 1.0 - thumbtippos.distance_to(middletippos)/0.04))
-
+		$ControllerRight.visible = false
 		$HandRightOVR.transform = Right_hand.transform  
 		processtoovrhand(Right_hand, $HandRightOVR/ovr_right_hand_model/ArmatureRight/Skeleton, false)
-
-		if not rightthumbindextouched:
-			rightthumbindextouched = (thumbtippos.distance_to(indextippos) < 0.01)
-			rightthumbindexjusttouched = rightthumbindextouched
-		else:
-			rightthumbindextouched = (thumbtippos.distance_to(indextippos) < 0.02)
-
+		$HandRightOVR.visible = true
+	else:
+		$HandLeftOVR.visible = false
+		$ControllerLeft.visible = false
 
 func setpaddlebody(active):
-	$HandRight/PaddleBody.visible = active
-	$HandRight/PaddleBody/CollisionShape.disabled = not active
+	$ControllerRight/PaddleBody.visible = active
+	$ControllerRight/PaddleBody/CollisionShape.disabled = not active
 
 func avatartoframedata():
+	var chleft = $HandLeftOVR if $HandLeftOVR.visible else $ControllerLeft
+	var chright = $HandRightOVR if $HandRightOVR.visible else $ControllerRight
 
 	var fd = {  NCONSTANTS2.CFI_VRORIGIN_POSITION: transform.origin, 
 				NCONSTANTS2.CFI_VRORIGIN_ROTATION: transform.basis.get_rotation_quat(), 
 				NCONSTANTS2.CFI_VRHEAD_POSITION: $HeadCam.transform.origin, 
 				NCONSTANTS2.CFI_VRHEAD_ROTATION: $HeadCam.transform.basis.get_rotation_quat(), 
-				NCONSTANTS2.CFI_VRHANDLEFT_POSITION: $HandLeft.transform.origin, 
-				NCONSTANTS2.CFI_VRHANDLEFT_ROTATION: $HandLeft.transform.basis.get_rotation_quat(), 
-				NCONSTANTS2.CFI_VRHANDRIGHT_POSITION: $HandRight.transform.origin, 
-				NCONSTANTS2.CFI_VRHANDRIGHT_ROTATION: $HandRight.transform.basis.get_rotation_quat(),
-				NCONSTANTS2.CFI_VRHANDLEFT_POSE: Vector2($HandLeft/LeftHand/AnimationTree.get("parameters/Grip/blend_amount"), 
-														 $HandLeft/LeftHand/AnimationTree.get("parameters/Trigger/blend_amount")),
-				NCONSTANTS2.CFI_VRHANDRIGHT_POSE: Vector2($HandRight/RightHand/AnimationTree.get("parameters/Grip/blend_amount"), 
-														  $HandRight/RightHand/AnimationTree.get("parameters/Trigger/blend_amount")),
+				
+				NCONSTANTS2.CFI_VRHANDLEFT_POSITION: chleft.transform.origin, 
+				NCONSTANTS2.CFI_VRHANDLEFT_ROTATION: chleft.transform.basis.get_rotation_quat(), 
+				NCONSTANTS2.CFI_VRHANDRIGHT_POSITION: chright.transform.origin, 
+				NCONSTANTS2.CFI_VRHANDRIGHT_ROTATION: chright.transform.basis.get_rotation_quat(),
 
-				NCONSTANTS2.CFI_VRHANDRIGHT_PADDLEBODY: $HandRight/PaddleBody.visible
+				NCONSTANTS2.CFI_VRHANDRIGHT_PADDLEBODY: $ControllerRight/PaddleBody.visible
 			 }
 	return fd
 
@@ -131,14 +123,8 @@ func overwritetranform(orgtransform, rot, pos):
 func framedatatoavatar(fd):
 	transform = overwritetranform(transform, fd.get(NCONSTANTS2.CFI_VRORIGIN_ROTATION), fd.get(NCONSTANTS2.CFI_VRORIGIN_POSITION))
 	$HeadCam.transform = overwritetranform($HeadCam.transform, fd.get(NCONSTANTS2.CFI_VRHEAD_ROTATION), fd.get(NCONSTANTS2.CFI_VRHEAD_POSITION))
-	$HandLeft.transform = overwritetranform($HandLeft.transform, fd.get(NCONSTANTS2.CFI_VRHANDLEFT_ROTATION), fd.get(NCONSTANTS2.CFI_VRHANDLEFT_POSITION))
-	$HandRight.transform = overwritetranform($HandRight.transform, fd.get(NCONSTANTS2.CFI_VRHANDRIGHT_ROTATION), fd.get(NCONSTANTS2.CFI_VRHANDRIGHT_POSITION))
-	if fd.has(NCONSTANTS2.CFI_VRHANDLEFT_POSE):
-		$HandLeft/LeftHand/AnimationTree.set("parameters/Grip/blend_amount", fd[NCONSTANTS2.CFI_VRHANDLEFT_POSE].x) 
-		$HandLeft/LeftHand/AnimationTree.set("parameters/Trigger/blend_amount", fd[NCONSTANTS2.CFI_VRHANDLEFT_POSE].y) 
-	if fd.has(NCONSTANTS2.CFI_VRHANDRIGHT_POSE):
-		$HandRight/RightHand/AnimationTree.set("parameters/Grip/blend_amount", fd[NCONSTANTS2.CFI_VRHANDRIGHT_POSE].x) 
-		$HandRight/RightHand/AnimationTree.set("parameters/Trigger/blend_amount", fd[NCONSTANTS2.CFI_VRHANDRIGHT_POSE].y) 
+	$ControllerLeft.transform = overwritetranform($ControllerLeft.transform, fd.get(NCONSTANTS2.CFI_VRHANDLEFT_ROTATION), fd.get(NCONSTANTS2.CFI_VRHANDLEFT_POSITION))
+	$ControllerRight.transform = overwritetranform($ControllerRight.transform, fd.get(NCONSTANTS2.CFI_VRHANDRIGHT_ROTATION), fd.get(NCONSTANTS2.CFI_VRHANDRIGHT_POSITION))
 	if fd.has(NCONSTANTS2.CFI_VRHANDRIGHT_PADDLEBODY):
 		print("remote setpaddlebody ", fd[NCONSTANTS2.CFI_VRHANDRIGHT_PADDLEBODY])
 		setpaddlebody(fd[NCONSTANTS2.CFI_VRHANDRIGHT_PADDLEBODY])
