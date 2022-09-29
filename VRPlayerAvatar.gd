95198ce16bfd764eed4ca3e07559c372cba5bb0c
Julian Todd <julian@goatchurch.org.uk>
2022-09-29 19:12:55 +0100

    hand tracking info now copies across to doppelganger

--- VRPlayerAvatar.gd
+++ VRPlayerAvatar.gd
@@ -61,6 +61,9 @@ func avatartoframedata():
 				NCONSTANTS2.CFI_VRORIGIN_ROTATION: transform.basis.get_rotation_quat(), 
 				NCONSTANTS2.CFI_VRHEAD_POSITION: $HeadCam.transform.origin, 
 				NCONSTANTS2.CFI_VRHEAD_ROTATION: $HeadCam.transform.basis.get_rotation_quat(), 
+
+				NCONSTANTS2.CFI_VRHANDCONTROLLERLEFT_FADE: (-1.0 if $ovr_left_hand_model.visible else (1.0 if $ControllerLeft.visible else 0.0)), 
+				NCONSTANTS2.CFI_VRHANDCONTROLLERRIGHT_FADE: (-1.0 if $ovr_right_hand_model.visible else (1.0 if $ControllerRight.visible else 0.0)),
 				
 				NCONSTANTS2.CFI_VRHANDLEFT_POSITION: chleft.transform.origin, 
 				NCONSTANTS2.CFI_VRHANDLEFT_ROTATION: chleft.transform.basis.get_rotation_quat(), 
@@ -69,6 +72,16 @@ func avatartoframedata():
 
 				NCONSTANTS2.CFI_VRHANDRIGHT_PADDLEBODY: $ControllerRight/PaddleBody.visible
 			 }
+
+	if $ovr_left_hand_model.visible:
+		var skel = ovrhandleftrestdata["skel"]
+		for i in range(23):
+			fd[NCONSTANTS2.CFI_VRHANDLEFT_BONE_ROTATIONS+i] = skel.get_bone_pose(i).basis.get_rotation_quat()
+	if $ovr_right_hand_model.visible:
+		var skel = ovrhandrightrestdata["skel"]
+		for i in range(23):
+			fd[NCONSTANTS2.CFI_VRHANDRIGHT_BONE_ROTATIONS+i] = skel.get_bone_pose(i).basis.get_rotation_quat()
+
 	return fd
 
 func overwritetranform(orgtransform, rot, pos):
@@ -83,11 +96,42 @@ func overwritetranform(orgtransform, rot, pos):
 func framedatatoavatar(fd):
 	transform = overwritetranform(transform, fd.get(NCONSTANTS2.CFI_VRORIGIN_ROTATION), fd.get(NCONSTANTS2.CFI_VRORIGIN_POSITION))
 	$HeadCam.transform = overwritetranform($HeadCam.transform, fd.get(NCONSTANTS2.CFI_VRHEAD_ROTATION), fd.get(NCONSTANTS2.CFI_VRHEAD_POSITION))
-	$ControllerLeft.transform = overwritetranform($ControllerLeft.transform, fd.get(NCONSTANTS2.CFI_VRHANDLEFT_ROTATION), fd.get(NCONSTANTS2.CFI_VRHANDLEFT_POSITION))
-	$ControllerRight.transform = overwritetranform($ControllerRight.transform, fd.get(NCONSTANTS2.CFI_VRHANDRIGHT_ROTATION), fd.get(NCONSTANTS2.CFI_VRHANDRIGHT_POSITION))
+
+	if fd.has(NCONSTANTS2.CFI_VRHANDCONTROLLERLEFT_FADE):
+		var hcleftfade = fd.get(NCONSTANTS2.CFI_VRHANDCONTROLLERLEFT_FADE)
+		$ControllerLeft.visible = (hcleftfade > 0.0)
+		$ovr_left_hand_model.visible = (hcleftfade < 0.0)
+	if fd.get(NCONSTANTS2.CFI_VRHANDCONTROLLERRIGHT_FADE):
+		var hcrightfade = fd.get(NCONSTANTS2.CFI_VRHANDCONTROLLERRIGHT_FADE)
+		$ControllerRight.visible = (hcrightfade > 0.0)
+		$ovr_right_hand_model.visible = (hcrightfade < 0.0)
+		
+	if $ovr_left_hand_model.visible:
+		$ovr_left_hand_model.transform = overwritetranform($ovr_left_hand_model.transform, fd.get(NCONSTANTS2.CFI_VRHANDLEFT_ROTATION), fd.get(NCONSTANTS2.CFI_VRHANDLEFT_POSITION))
+		var skel = $ovr_left_hand_model/ArmatureLeft/Skeleton
+		for i in range(23):
+			var frot = fd.get(NCONSTANTS2.CFI_VRHANDLEFT_BONE_ROTATIONS+i)
+			if frot != null:
+				skel.set_bone_pose(i, Transform(frot))
+	elif $ControllerLeft.visible:
+		$ControllerLeft.transform = overwritetranform($ControllerLeft.transform, fd.get(NCONSTANTS2.CFI_VRHANDLEFT_ROTATION), fd.get(NCONSTANTS2.CFI_VRHANDLEFT_POSITION))
+
+	if $ovr_right_hand_model.visible:
+		$ovr_right_hand_model.transform = overwritetranform($ovr_right_hand_model.transform, fd.get(NCONSTANTS2.CFI_VRHANDRIGHT_ROTATION), fd.get(NCONSTANTS2.CFI_VRHANDRIGHT_POSITION))
+		var skel = $ovr_right_hand_model/ArmatureRight/Skeleton
+		for i in range(23):
+			var frot = fd.get(NCONSTANTS2.CFI_VRHANDRIGHT_BONE_ROTATIONS+i)
+			if frot != null:
+				skel.set_bone_pose(i, Transform(frot))
+	elif $ControllerRight.visible:
+		$ControllerRight.transform = overwritetranform($ControllerRight.transform, fd.get(NCONSTANTS2.CFI_VRHANDRIGHT_ROTATION), fd.get(NCONSTANTS2.CFI_VRHANDRIGHT_POSITION))
+
+		
 	if fd.has(NCONSTANTS2.CFI_VRHANDRIGHT_PADDLEBODY):
 		print("remote setpaddlebody ", fd[NCONSTANTS2.CFI_VRHANDRIGHT_PADDLEBODY])
 		setpaddlebody(fd[NCONSTANTS2.CFI_VRHANDRIGHT_PADDLEBODY])
+
+
 		
 var possibleusernames = ["Alice", "Beth", "Cath", "Dan", "Earl", "Fred", "George", "Harry", "Ivan", "John", "Kevin", "Larry", "Martin", "Oliver", "Peter", "Quentin", "Robert", "Samuel", "Thomas", "Ulrik", "Victor", "Wayne", "Xavier", "Youngs", "Zephir"]
 func initavatarlocal():
