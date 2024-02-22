7bc0246d287ae5384d074183f93173776b145f85
Julian Todd <julian@goatchurch.org.uk>
2024-02-22 17:15:29 +0000

    moving verlet

--- manualposemaker/ManualPoseMaker.gd
+++ manualposemaker/ManualPoseMaker.gd
@@ -2,6 +2,42 @@ extends Node3D
 
 @onready var skel = get_node("../redoran3/MonsterArmature/Skeleton3D")
 
+
+class VerletJoint:
+	var pj
+	var pjheadtail
+	var pjlen
+	var pjbone
+	var j
+	var jheadtail
+	var jlen
+	var jbone
+	
+	func init(lj, skel, posescene):
+		j = lj
+		jbone = posescene.get_node("bonestick%d" % j)
+		jheadtail = true
+		pj = skel.get_bone_parent(j)
+		assert (pj != -1)
+		pjbone = posescene.get_node("bonestick%d" % pj)
+		pjheadtail = false
+		var ppj = skel.get_bone_parent(pj)
+		var ptransform = skel.global_transform*skel.get_bone_global_pose(pj)
+		var btransform = ptransform*skel.get_bone_pose(j) 
+		var pptransform = skel.global_transform*(skel.get_bone_global_pose(ppj) if ppj != -1 else Transform3D())
+		pjlen = (ptransform.origin - pptransform.origin).length()
+		jlen = (btransform.origin - ptransform.origin).length()
+
+	func movebone(moveparent):
+		var jointpos = jbone.transform.origin - jbone.transform.basis.y*(jlen/2)
+		var pjointpos = pjbone.transform.origin + pjbone.transform.basis.y*(pjlen/2)
+		if moveparent:
+			pjbone.transform.origin += jointpos - pjointpos
+		else:
+			jbone.transform.origin += pjointpos - jointpos 
+
+var vjoints = [ ]
+
 static func rotationtoalign(a, b):
 	var axis = a.cross(b).normalized()
 	if (axis.length_squared() != 0):
@@ -10,6 +46,8 @@ static func rotationtoalign(a, b):
 		var angle_rads = acos(dot)
 		return Basis(axis, angle_rads)
 	return Basis()
+
+
 
 func updatebones():
 	var gs = skel.global_transform
@@ -28,25 +66,65 @@ func updatebones():
 		#byalign = byalign * (Basis().scaled(Vector3(1,vpblen,1)))
 		rj.transform = Transform3D(parenttransform.basis.orthonormalized()*byalign, parenttransform.origin + vpb*0.5)
 		if pj != -1:
-			var joint = rj.get_node("Generic6DOFJoint3D")
-			joint.transform.origin = Vector3(0,-vpblen/2,0)
-			joint.node_a = NodePath("..")
-			joint.node_b = NodePath("../../bonestick%d" % pj)
+			var joint = rj.get_node_or_null("Generic6DOFJoint3D")
+			if joint:
+				joint.transform.origin = Vector3(0,-vpblen/2,0)
+				joint.node_a = NodePath("..")
+				joint.node_b = NodePath("../../bonestick%d" % pj)
 		add_child(rj)
 		
+	for vjoint in vjoints:
+		vjoint.movebone(true)
+
+
 	skel.reset_bone_poses()
-		
+
+var pickedbones = [ ]
+
+func onbonestickpickedup(b):
+	print("pickup ", b.get_path())
+	pickedbones.append(b.name.to_int())
+func onbonestickdropped(b):
+	print("drop ", b.name, "  ", b.name.to_int()*1000)
+	pickedbones.erase(b.name.to_int())
+
+func verletrun():
+	for j in pickedbones:
+		var bonesvisited = pickedbones.duplicate()
+		var bonesprocessing = [ j ]
+		while len(bonesprocessing) != 0:
+			var lj = bonesprocessing.pop_front()
+			var vjs = [ ]
+			for vjoint in vjoints:
+				if vjoint.j == lj and not bonesvisited.has(vjoint.pj):
+					vjoint.movebone(true)
+					vjs.append(vjoint.pj)
+				if vjoint.pj == lj and not bonesvisited.has(vjoint.j):
+					vjoint.movebone(false)
+					vjs.append(vjoint.j)
+			bonesprocessing.append_array(vjs)
+			bonesvisited.append_array(vjs)
+
+func _process(delta):
+	verletrun()
+
 func _ready():
-	
-	var bonestickscene = load("res://manualposemaker/pickablebonestick.tscn")
+	#var bonestickscene = load("res://manualposemaker/pickablebonestick.tscn")
+	var bonestickscene = load("res://manualposemaker/vpickablebonestick.tscn")
 	var bonesticknode = $ExampleBoneStick
 	#remove_child(bonesticknode)
 	for j in range(skel.get_bone_count()):
 		var rj = bonestickscene.instantiate()
-		rj.get_node("CollisionShape3D").shape = rj.get_node("CollisionShape3D").shape.duplicate()
-		rj.get_node("MeshInstance3D").mesh = rj.get_node("MeshInstance3D").mesh.duplicate()
+		#rj.get_node("CollisionShape3D").shape = rj.get_node("CollisionShape3D").shape.duplicate()
+		#rj.get_node("MeshInstance3D").mesh = rj.get_node("MeshInstance3D").mesh.duplicate()
 		rj.name = "bonestick%d" % j
+		rj.picked_up.connect(onbonestickpickedup)
+		rj.dropped.connect(onbonestickdropped)
 		add_child(rj)
+		if skel.get_bone_parent(j) != -1:
+			var vjoint = VerletJoint.new()
+			vjoint.init(j, skel, self)
+			vjoints.push_back(vjoint)
 	updatebones()
 
 
@@ -65,7 +143,3 @@ func _input(event):
 				print("removed and added ", X)
 
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
