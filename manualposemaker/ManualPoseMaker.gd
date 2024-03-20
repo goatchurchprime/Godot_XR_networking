7e28173f5e182df2b08e93d623a69af8004506c7
Julian Todd <julian@goatchurch.org.uk>
2024-03-20 13:25:20 +0000

    now forces hinges to be pinned

--- manualposemaker/ManualPoseMaker.gd
+++ manualposemaker/ManualPoseMaker.gd
@@ -48,8 +48,9 @@ class BoneUnit:
 	var boneindex : int
 	var bonestick : Node
 	var bonemass : float
-	var nextboneunitjoints = [ ] # [ { jointvector, nextboneunit, nextboneunitjoint } ]
-
+	var nextboneunitjoints = [ ] # [ { jointvector, nextboneunit, nextboneunitjoint } ]  (0th joint is to the parent)
+	var hingetoparentaxis = null 
+	
 	var bonequat0 : Quaternion
 	var bonecentre0 : Vector3
 
@@ -74,6 +75,9 @@ class BoneJointEl:
 	var overridepropbonequat : Quaternion
 	var gradE : Vector3
 
+	var isconstorientation : bool
+	var hingejointaxis = null
+
 	func derivebointjointcentre(prevquat, prevcentre, quat):
 		var jointpos = prevcentre + prevquat*prevbonejointvector
 		return jointpos - quat*incomingbonejointvector
@@ -81,7 +85,6 @@ class BoneJointEl:
 	
 var bonejointsequence = null
 var fixedboneslist = [ ]
-var bonejointseqstartticks = 0
 
 func calcbonecentresfromquatsE():
 	var Ergsum = 0.0
@@ -96,8 +99,13 @@ func calcbonecentresfromquatsE():
 		else:
 			prevcentre = bonejointsequence[bje.prevbonejointel].propbonecentre
 			prevquat = bonejointsequence[bje.prevbonejointel].propbonequat
-		bje.propbonecentre = bje.derivebointjointcentre(prevquat, prevcentre, bje.propbonequat)
 		var bu = boneunits[bje.boneunitindex]
+		if bje.isconstorientation:
+			bje.propbonequat = bu.bonequat0
+
+#		if bje.hingejointaxis == null:			
+			
+		bje.propbonecentre = bje.derivebointjointcentre(prevquat, prevcentre, bje.propbonequat)
 		Ergsum += bu.bonemass*(bje.propbonecentre - bu.bonecentre0).length_squared()
 	return Ergsum
 
@@ -149,9 +157,16 @@ func calcEgraddelta(delta):
 			prevquat = bonejointsequence[bje.prevbonejointel].overridepropbonequat
 
 		var v = bje.gradE*delta
-		var addpropbonequat = Quaternion(v.x, v.y, v.z, sqrt(1.0 - v.length_squared()))
-		bje.overridepropbonequat = bje.propbonequat*addpropbonequat
+
+		if bje.hingejointaxis == null:
+			var addpropbonequat = Quaternion(v.x, v.y, v.z, sqrt(1.0 - v.length_squared()))
+			bje.overridepropbonequat = bje.propbonequat*addpropbonequat
+		else:
+			var addpropbonequat = Quaternion(v.x, v.y, v.z, sqrt(1.0 - v.length_squared()))
+			bje.overridepropbonequat = bje.propbonequat*addpropbonequat
+
 		bje.overridepropbonecentre = bje.derivebointjointcentre(prevquat, prevcentre, bje.overridepropbonequat)
+		
 		var bu = boneunits[bje.boneunitindex]
 		Ergsum += bu.bonemass*(bje.overridepropbonecentre - bu.bonecentre0).length_squared()
 	return Ergsum
@@ -174,8 +189,48 @@ func setboneposefromunits():
 		var btrans = ptrans.inverse()*jtrans
 		skel.set_bone_pose_position(j, btrans.origin/sca)
 		skel.set_bone_pose_rotation(j, btrans.basis.get_rotation_quaternion())
-	
-func _ready():
+
+func forcenbonequat0ashinges():
+	for i in range(len(bonejointsequence)):
+		var bje = bonejointsequence[i]
+		var prevcentre
+		var prevquat
+		if bje.prevbonejointel == -1:
+			var bu0 = boneunits[bonejointsequence[0].prevboneunitindex]
+			prevcentre = bu0.bonecentre0
+			prevquat = bu0.bonequat0
+		else:
+			prevcentre = bonejointsequence[bje.prevbonejointel].propbonecentre
+			prevquat = bonejointsequence[bje.prevbonejointel].propbonequat
+		var bu = boneunits[bje.boneunitindex]
+		#if bje.isconstorientation:
+		#	bje.propbonequat = bu.bonequat0
+			#assert (bje.hingejointaxis == null)
+		if bje.hingejointaxis != null:
+			bje.propbonequat = bu.bonequat0
+			# solve prevquat*Quat(bje.hingejointaxis*sina, cosa) = bje.overridepropbonequat  where a = angle/2
+			var perp = (Vector3(1,1,1).cross(bje.hingejointaxis)).normalized()
+			var perp2 = bje.hingejointaxis.cross(perp)
+			var qh = prevquat.inverse()*bje.propbonequat
+			var qhR = qh*perp
+			var q2c = perp.dot(qhR)
+			var ang = acos(q2c)*sign(perp2.dot(qhR))
+			#print("-- angwas ", rad_to_deg(ang))
+			var qc = cos(ang/2)
+			var qs = sin(ang/2)
+			var h = bje.hingejointaxis
+			var qq = Quaternion(bje.hingejointaxis.x*qs, bje.hingejointaxis.y*qs, bje.hingejointaxis.z*qs, qc)
+			#print(" ang ", rad_to_deg(ang), " qq ", q2c, "  ", perp.dot(qq*perp), "  ", bje.hingejointaxis.dot(qq*bje.hingejointaxis))
+			#print("  ", prevquat*qq, bje.propbonequat)
+			bje.propbonequat = prevquat*qq
+		bje.propbonecentre = bje.derivebointjointcentre(prevquat, prevcentre, bje.propbonequat)
+
+
+func createboneunitsfromskeleton():	
+	var regex = RegEx.new()
+	#regex.compile("(foot|leg|hand) \\.[LR]$")
+	regex.compile("(foot|leg|fingers) \\.[LR]$")   # The hand has a twist between the bones
+
 	for j in range(skel.get_bone_count()):
 		var bu = BoneUnit.new()
 		bu.boneindex = j
@@ -212,6 +267,22 @@ func _ready():
 
 	for j in range(len(boneunits)):
 		var bu = boneunits[j]
+		if regex.search(skel.get_bone_name(j)) != null:
+			var bup = boneunits[skel.get_bone_parent(j)]
+			var quat = bup.bonequat0.inverse() * bu.bonequat0
+			print(skel.get_bone_name(j), " ", quat)
+			if skel.get_bone_name(j).contains("hand") or skel.get_bone_name(j).contains("fingers"):
+				bu.hingetoparentaxis = Vector3(0,0,1)
+			else:
+				bu.hingetoparentaxis = Vector3(1,0,0)
+		else:
+			bu.hingetoparentaxis = null
+
+
+
+func createbonesticksfromboneunits():
+	for j in range(len(boneunits)):
+		var bu = boneunits[j]
 		if bu.bonemass != 0.0:
 			var rj = bonestickscene.instantiate()
 			rj.name = "bonestick%d" % j
@@ -226,7 +297,10 @@ func _ready():
 			bu.bonestick = rj
 			bu.bonestick.transform = Transform3D(bu.bonequat0, bu.bonecentre0)
 
-	setboneposefromunits()
+func _ready():
+	createboneunitsfromskeleton()
+	createbonesticksfromboneunits()
+	#setboneposefromunits()
 
 
 func seebonequatscentres(bapply):
@@ -239,24 +313,28 @@ func seebonequatscentres(bapply):
 			bu.bonequat0 = bje.propbonequat
 			bu.bonecentre0 = bje.propbonecentre
 
-
 func calcnumericalgradient(eps):
 	var qeps = sqrt(1.0 - eps*eps)
 	var sumgradEsq = 0.0
 	for k in range(len(bonejointsequence)):
-		var gx = calcEsinglequat(k, Quaternion(eps, 0, 0, qeps))
-		var gy = calcEsinglequat(k, Quaternion(0, eps, 0, qeps))
-		var gz = calcEsinglequat(k, Quaternion(0, 0, eps, qeps))
-		var gradE = Vector3(gx, gy, gz)/eps
-		bonejointsequence[k].gradE = gradE
-		sumgradEsq += gradE.length_squared()
+		var bjeK = bonejointsequence[k]
+		if bjeK.isconstorientation:
+			bonejointsequence[k].gradE = Vector3(0,0,0)
+		else:
+			var gx = calcEsinglequat(k, Quaternion(eps, 0, 0, qeps))
+			var gy = calcEsinglequat(k, Quaternion(0, eps, 0, qeps))
+			var gz = calcEsinglequat(k, Quaternion(0, 0, eps, qeps))
+			var gradE = Vector3(gx, gy, gz)/eps
+			bonejointsequence[k].gradE = gradE
+			sumgradEsq += gradE.length_squared()
+			
 	return sumgradEsq
+
 
 
 func derivejointsequence(jstart):
 	bonejointsequence = [ ]
 	fixedboneslist = [ jstart ]
-	bonejointseqstartticks = Time.get_ticks_usec()
 
 	var boneunitsvisited = [ jstart ]
 	var boneunitsvisitedProcessed = 0
@@ -264,48 +342,78 @@ func derivejointsequence(jstart):
 		var j = boneunitsvisited[boneunitsvisitedProcessed]
 		var bu = boneunits[j]
 		for i in range(len(bu.nextboneunitjoints)):
-			if bu.nextboneunitjoints[i].has("nextboneunit"):
-				var bnti = bu.nextboneunitjoints[i]
-				var nj = bnti["nextboneunit"]
-				if not boneunitsvisited.has(nj):
-					var bje = BoneJointEl.new()
-					bje.prevboneunitindex = j
-					bje.prevjointindex = i
-					bje.prevbonejointvector = bnti["jointvector"]
+			if not bu.nextboneunitjoints[i].has("nextboneunit"):
+				continue
+			var bnti = bu.nextboneunitjoints[i]
+			var nj = bnti["nextboneunit"]
+			if boneunitsvisited.has(nj):
+				continue
 
-					var bunext = boneunits[nj]
-					bje.incomingjointindex = bu.nextboneunitjoints[i]["nextboneunitjoint"]
-					var bntinext = bunext.nextboneunitjoints[bje.incomingjointindex]
-					bje.boneunitindex = nj
-					bje.incomingbonejointvector = bntinext["jointvector"]
-					assert (bntinext["nextboneunit"] == j and bntinext["nextboneunitjoint"] == i)
-		
-					# initialize at original position
-					bje.propbonequat = bunext.bonequat0
-					bje.propbonecentre = bunext.bonecentre0
+			var bje = BoneJointEl.new()
+			bje.prevboneunitindex = j
+			bje.prevjointindex = i
+			bje.prevbonejointvector = bnti["jointvector"]
 
-					var backbonejointel = len(bonejointsequence) - 1
-					while backbonejointel >= 0:
-						if bonejointsequence[backbonejointel].boneunitindex == bje.prevboneunitindex:
-							break
-						backbonejointel -= 1
-					bje.prevbonejointel = backbonejointel
-					bje.nextboneellist = [ ]
-					var cbonejointel = len(bonejointsequence)
-					while backbonejointel >= 0:
-						bonejointsequence[backbonejointel].nextboneellist.push_back(cbonejointel)
-						backbonejointel = bonejointsequence[backbonejointel].prevbonejointel
-					bonejointsequence.push_back(bje)
-					boneunitsvisited.push_back(nj)
+			var bunext = boneunits[nj]
+			bje.incomingjointindex = bu.nextboneunitjoints[i]["nextboneunitjoint"]
+			var bntinext = bunext.nextboneunitjoints[bje.incomingjointindex]
+			bje.boneunitindex = nj
+			bje.incomingbonejointvector = bntinext["jointvector"]
+			assert (bntinext["nextboneunit"] == j and bntinext["nextboneunitjoint"] == i)
+
+			# mark hinge joints
+			if bje.incomingjointindex == 0:
+				assert (bje.prevboneunitindex == skel.get_bone_parent(bje.boneunitindex))
+				bje.hingejointaxis = bunext.hingetoparentaxis
+			elif bje.prevjointindex == 0:
+				assert (bje.boneunitindex == skel.get_bone_parent(bje.prevboneunitindex))
+				bje.hingejointaxis = bu.hingetoparentaxis
+			assert (bje.hingejointaxis == null or bje.hingejointaxis.is_normalized())
+
+			# initialize at original position
+			bje.propbonequat = bunext.bonequat0
+			bje.propbonecentre = bunext.bonecentre0
+
+			# back joint pointers and forward dependence joints list
+			var backbonejointel = len(bonejointsequence) - 1
+			while backbonejointel >= 0:
+				if bonejointsequence[backbonejointel].boneunitindex == bje.prevboneunitindex:
+					break
+				backbonejointel -= 1
+			bje.prevbonejointel = backbonejointel
+			bje.nextboneellist = [ ]
+			var cbonejointel = len(bonejointsequence)
+			while backbonejointel >= 0:
+				bonejointsequence[backbonejointel].nextboneellist.push_back(cbonejointel)
+				backbonejointel = bonejointsequence[backbonejointel].prevbonejointel
+
+			bonejointsequence.push_back(bje)
+			boneunitsvisited.push_back(nj)
+
 		boneunitsvisitedProcessed += 1
+	assert (bonejointsequence[0].prevboneunitindex == jstart)
 
 
 
+func setisconstorientation(constrainedbones):
+	for i in range(len(bonejointsequence)):
+		var bje = bonejointsequence[i]
+		assert (bonejointsequence[0].prevboneunitindex != bje.boneunitindex)
+		bje.isconstorientation = constrainedbones.has(bje.boneunitindex)
+
+
+var bworkingsimulation = false
 func minenergymove(pickedbones):
-	var jmoved = pickedbones[0]
-	derivejointsequence(jmoved)
-	boneunits[jmoved].bonequat0 = boneunits[jmoved].bonestick.transform.basis.get_rotation_quaternion()
-	boneunits[jmoved].bonecentre0 = boneunits[jmoved].bonestick.transform.origin
+	var jstart = pickedbones[0]
+	
+	bworkingsimulation = true
+	derivejointsequence(jstart)  # makes bonejointsequence
+	setisconstorientation(pickedbones)
+
+	for i in pickedbones:
+		var bu = boneunits[i]
+		bu.bonequat0 = bu.bonestick.transform.basis.get_rotation_quaternion()
+		bu.bonecentre0 = bu.bonestick.transform.origin
 
 	var t0 = Time.get_ticks_usec()
 	for i in range(10):
@@ -313,15 +421,16 @@ func minenergymove(pickedbones):
 		var t1 = Time.get_ticks_usec()
 		if not bstepped:
 			break
-		#seebonequatscentres(false)
-		#await get_tree().create_timer(0.1).timeout
-		print("Ticks ", t1 - t0)
+		seebonequatscentres(false)
+		print("DDEd ", DDEd)
+		await get_tree().create_timer(0.1).timeout
 		t0 = t1
 	seebonequatscentres(true)
 	setboneposefromunits()
+	bworkingsimulation = false
 	bonejointsequence = null
 
-
+var DDEd = 0
 func makegradstep(Ni):
 	var E0 = calcbonecentresfromquatsE()
 	var sumgradEsq = calcnumericalgradient(0.0001)
@@ -335,6 +444,8 @@ func makegradstep(Ni):
 	
 	for ii in range(10):
 		var DEd = calcEgraddelta(-delta)
+		#print(" ", ii, " ", delta, "  ", DEd, "  E0 ", E0)
+		DDEd = DEd
 		if DEd < E0 - delta*c*sumgradEsq:
 			for i in range(len(bonejointsequence)):
 				var bje = bonejointsequence[i]
@@ -342,7 +453,7 @@ func makegradstep(Ni):
 				bje.propbonecentre = bje.overridepropbonecentre
 			return true
 		delta = delta*tau
-	print("Bailing out at step ", Ni)
+	#print("Bailing out at step ", Ni, " E0 ", E0)
 	return false
 
 var pickedbones = [ ]
@@ -351,6 +462,7 @@ func onbonestickpickedup(b):
 	var jpicked = b.name.to_int()
 	print("pickup ", b.get_path())
 	pickedbones.push_back(jpicked)
+	pickedboneschanged = true
 	allpickedbones.erase(jpicked)
 	allpickedbones.push_back(jpicked)
 
@@ -359,41 +471,65 @@ func onbonestickdropped(b):
 	print("drop ", b.name, "  ", b.name.to_int()*1000)
 	var jdropped = b.name.to_int()
 	pickedbones.erase(jdropped)
+	pickedboneschanged = true
 	if len(pickedbones) == 0:
 		if bupdatefinalondrop:
 			minenergymove(allpickedbones)
 		allpickedbones = [ ]
 
 var Pjpickedbone = -1
+var pickedboneschanged = false
+var bonejointseqstartticks = 0
+var bonejointgradsteps = 0
 func _physics_process(delta):
 	if bupdatefinalondrop:
 		return
+	if bworkingsimulation:
+		return
 	var physt0 = Time.get_ticks_usec()
-	if len(allpickedbones) and allpickedbones[0] != Pjpickedbone:
-		Pjpickedbone = allpickedbones[0]
+	if len(pickedbones) == 0:
+		Pjpickedbone = -1
+	elif pickedbones[0] != Pjpickedbone:
+		Pjpickedbone = pickedbones[0]
+		print(" pprocess Pjpickedbone ", Pjpickedbone)
 		derivejointsequence(Pjpickedbone)
 		bonejointseqstartticks = physt0
-		
-	if allpickedbones.has(Pjpickedbone):
-		boneunits[Pjpickedbone].bonequat0 = boneunits[Pjpickedbone].bonestick.transform.basis.get_rotation_quaternion()
-		boneunits[Pjpickedbone].bonecentre0 = boneunits[Pjpickedbone].bonestick.transform.origin
+		bonejointgradsteps = 0
+	if pickedboneschanged and bonejointsequence != null:
+		setisconstorientation(pickedbones)
+
+	for i in range(len(pickedbones)):
+		var jpickedbone = pickedbones[i]
+		var bu = boneunits[jpickedbone]
+		bu.bonequat0 = bu.bonestick.transform.basis.get_rotation_quaternion()
+		bu.bonecentre0 = bu.bonestick.transform.origin
 		
 	if bonejointsequence != null:
 		var bEndIteration = false
 		for Ci in range(10):
+			print(" make gradstep ", bonejointgradsteps)
 			var bstepped = makegradstep(Ci)
+			bonejointgradsteps += 1
 			if not bstepped:
+				bEndIteration = true
+				print("notstepped")
+				break
+			if bonejointgradsteps >= 15:
 				bEndIteration = true
 				break
 			var physt = Time.get_ticks_usec()
 			if physt - bonejointseqstartticks > 200000:
 				bEndIteration = true
 				break
-			if physt - physt0 > 20000:
+			if physt - physt0 > 8000:
 				break
 		if bEndIteration:
+			print(" setbonepose from units")
+			forcenbonequat0ashinges()
 			seebonequatscentres(true)
 			setboneposefromunits()
+			bonejointseqstartticks = Time.get_ticks_usec()
+			bonejointgradsteps = 0
 			if Pjpickedbone == -1:
 				bonejointsequence = null
 
@@ -405,16 +541,31 @@ func _input(event):
 			if event.keycode == KEY_P:
 				if Ddd < 3:
 					get_node("bonestick7").transform.origin.z += 0.1
+					get_node("bonestick7").transform = get_node("bonestick7").transform.rotated_local(Vector3(0,0,1), 0.1)
+#					get_node("bonestick17").transform.origin.y += 0.1
+					get_node("bonestick17").transform = get_node("bonestick17").transform.rotated_local(Vector3(1,0,0), 0.1)
 				else:
-					get_node("bonestick7").transform.origin.z += -0.01
+					get_node("bonestick7").transform = get_node("bonestick7").transform.rotated_local(Vector3(0,0,1), -0.1)
+#					get_node("bonestick7").transform.origin.z += -0.05
+#					get_node("bonestick17").transform.origin.y += -0.1
+					get_node("bonestick17").transform = get_node("bonestick17").transform.rotated_local(Vector3(1,0,0), 0.1)
 				Ddd += 1
-				minenergymove([7])
+				if not bworkingsimulation:
+					minenergymove([7, 17])
+	
+			if event.keycode == KEY_M:
+				derivejointsequence(7)
+				forcenbonequat0ashinges()
+				seebonequatscentres(true)
+				setboneposefromunits()
+				bonejointsequence = null
 				
 			if event.keycode == KEY_O:
 				var bonetomove = get_node("bonestick7")
 				onbonestickpickedup(bonetomove)
 				for i in range(10):
 					bonetomove.transform.origin.z += 0.01
+					print("moved ", i, " now awaiting ")
 					await get_tree().create_timer(0.1).timeout
 				onbonestickdropped(bonetomove)
 
