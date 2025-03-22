e4fa11d34e21519ce814a6e055185f54191ecf28
Julian Todd <julian@goatchurch.org.uk>
2025-03-22 20:15:26 +0000

    converting to the gd-plug system

---
+++ plug.gd
@@ -0,0 +1,7 @@
+extends "res://addons/gd-plug/plug.gd"
+
+func _plugging():
+	# plug("imjp94/gd-YAFSM") # By default, gd-plug will only install anything from "addons/" directory
+	# plug("imjp94/gd-YAFSM", {"include": ["addons/"]})
+	plug("GodotVR/godot-xr-tools", {"commit": "4d53140015ba5feb6f6ec07873f050311ffe1fdc"})
+	plug("Cafezinhu/godot-vr-simulator", {"commit": "5bedbbacf6fe40af10e0bfea99487c84387b19f3"})
